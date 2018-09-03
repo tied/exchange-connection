20a942760c181cb994f6e27c4857c3fcc9558f54
nik_aleks <sdfd>
2018-09-03 10:45:37 +0300

    prepare

---
+++ src/main/java/com/talmer/plugin/api/PluginSettingService.java
@@ -0,0 +1,11 @@
+package com.talmer.plugin.api;
+
+public interface PluginSettingService {
+    String getLogin();
+
+    void setLogin(String login);
+
+    String getPassword();
+
+    void setPassword(String password);
+}
